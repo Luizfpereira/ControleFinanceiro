d4f1acf4d6ac31087f3e83a9ca02a8ebd84bf0b2
luiz <luizpereira.mec@outlook.com>
2020-08-13 12:10:34 -0300

    postgresql

--- src/main/java/br/com/financeiro/controlefinanceiro/config/JPAProductionConfiguration.java
+++ src/main/java/br/com/financeiro/controlefinanceiro/config/JPAProductionConfiguration.java
@@ -32,7 +32,7 @@ public class JPAProductionConfiguration {
 		
 		
 		URI dbUrl = new URI(environment.getProperty("DATABASE_URL"));
-		dataSource.setUrl("jdbc:postgre://"+dbUrl.getHost()
+		dataSource.setUrl("jdbc:postgresql://"+dbUrl.getHost()
 		+":"+dbUrl.getPort()+dbUrl.getPath());
 		dataSource.setUsername(dbUrl.getUserInfo().split(":")[0]);
 		dataSource.setPassword(dbUrl.getUserInfo().split(":")[1]);
